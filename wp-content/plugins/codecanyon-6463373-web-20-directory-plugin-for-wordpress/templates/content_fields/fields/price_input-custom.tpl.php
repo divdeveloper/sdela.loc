08f6835e026becb78b327ee6b3e8532a6be6c105
zhuiks <zhuke@ukr.net>
2018-03-03 00:41:13 +0200

    little css fixes

--- wp-content/plugins/codecanyon-6463373-web-20-directory-plugin-for-wordpress/templates/content_fields/fields/price_input-custom.tpl.php
+++ wp-content/plugins/codecanyon-6463373-web-20-directory-plugin-for-wordpress/templates/content_fields/fields/price_input-custom.tpl.php
@@ -1,2 +1,2 @@
 <label class="control-label"><?php echo $content_field->description ? $content_field->description : $content_field->name; ?></label>
-<input type="text" name="w2dc-field-input-<?php echo $content_field->id; ?>" class="w2dc-field-input-price form-control" value="<?php echo esc_attr($content_field->value); ?>" size="4" />
+<input type="number" min="0" step="0.01" max="1000000" name="w2dc-field-input-<?php echo $content_field->id; ?>" class="w2dc-field-input-price form-control" value="<?php echo esc_attr($content_field->value); ?>" size="4" />
